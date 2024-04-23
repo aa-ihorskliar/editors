e372e1a7be0bcc3337121eea195c975377c60538
Ihor Skliar <ihor.skliar@agencyanalytics.com>
2024-04-23 17:19:50 -0400

    wip

--- src/editors/cke/CKEditorElement.tsx
+++ src/editors/cke/CKEditorElement.tsx
@@ -2,32 +2,30 @@ import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-const config = {
-    toolbar: [ 'bold', 'italic' ]
-};
+const config = {};
 
-export default function CKEditorElement({ content }: { content: string; }) {
+export default function CKEditorElement({ content }: { content: string }) {
   return (
-      <div className="App">
-        <h2>Using CKEditor&nbsp;5 build in React</h2>
-        <CKEditor
-            config={config}
-            editor={ClassicEditor}
-            data={content}
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onChange={(event) => {
-              console.log(event);
-            }}
-            onBlur={(event, editor) => {
-              console.log("Blur.", editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log("Focus.", editor);
-            }}
-        />
-      </div>
+    <div className="App">
+      <h2>Using CKEditor&nbsp;5 build in React</h2>
+      <CKEditor
+        config={config}
+        editor={ClassicEditor}
+        data={content}
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event) => {
+          console.log(event);
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+      />
+    </div>
   );
 }
