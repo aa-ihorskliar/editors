e11ebdc5d9955de131e3c0aef71e9275b0a7f383
Ihor Skliar <ihor.skliar@agencyanalytics.com>
2024-04-23 17:07:01 -0400

    wip

--- src/editors/cke/CKEditorElement.tsx
+++ src/editors/cke/CKEditorElement.tsx
@@ -1,112 +1,33 @@
 import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-// The editor creator to use.
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-
-import { Alignment } from '@ckeditor/ckeditor5-alignment';  // Importing the package.
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import {
-    Image,
-    ImageCaption,
-    ImageStyle,
-    ImageToolbar,
-    ImageUpload
-} from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const config = {
-  plugins: [
-    Alignment, // Adding the package to the list of plugins.
-    Autoformat,
-    BlockQuote,
-    Bold,
-    CloudServices,
-    Essentials,
-    Heading,
-    Image,
-    ImageCaption,
-    ImageStyle,
-    ImageToolbar,
-    ImageUpload,
-    Indent,
-    Italic,
-    Link,
-    List,
-    MediaEmbed,
-    Paragraph,
-    PasteFromOffice,
-    Table,
-    TableToolbar,
-    TextTransformation,
-  ],
-  toolbar: {
-    items: [
-      "alignment", // Displaying the proper UI element in the toolbar.
-      "heading",
-      "|",
-      "bold",
-      "italic",
-      "link",
-      "bulletedList",
-      "numberedList",
-      "|",
-      "outdent",
-      "indent",
-      "|",
-      "imageUpload",
-      "blockQuote",
-      "insertTable",
-      "mediaEmbed",
-      "undo",
-      "redo",
-    ],
-  },
-  language: "en",
-  image: {
-    toolbar: [
-      "imageTextAlternative",
-      "toggleImageCaption",
-      "imageStyle:inline",
-      "imageStyle:block",
-      "imageStyle:side",
-    ],
-  },
-  table: {
-    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
-  },
+    toolbar: [ 'bold', 'italic' ]
 };
 
-export default function CKEditorElement({ content }: { content: string }) {
+export default function CKEditorElement({ content }: { content: string; }) {
   return (
-    <CKEditor
-      editor={ClassicEditor}
-      data={content}
-      config={config}
-      onReady={(editor) => {
-        // You can store the "editor" and use when it is needed.
-        console.log("Editor is ready to use!", editor);
-      }}
-      onChange={(event) => {
-        console.log(event);
-      }}
-      onBlur={(event, editor) => {
-        console.log("Blur.", editor);
-      }}
-      onFocus={(event, editor) => {
-        console.log("Focus.", editor);
-      }}
-    />
+      <div className="App">
+        <h2>Using CKEditor&nbsp;5 build in React</h2>
+        <CKEditor
+            config={config}
+            editor={ClassicEditor}
+            data={content}
+            onReady={(editor) => {
+              // You can store the "editor" and use when it is needed.
+              console.log("Editor is ready to use!", editor);
+            }}
+            onChange={(event) => {
+              console.log(event);
+            }}
+            onBlur={(event, editor) => {
+              console.log("Blur.", editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log("Focus.", editor);
+            }}
+        />
+      </div>
   );
 }
