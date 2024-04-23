1c29838b47a79711b188dbfbd198e387f73a4a1a
Ihor Skliar <ihor.skliar@agencyanalytics.com>
2024-04-23 15:48:12 -0400

    c

--- src/editors/cke/CKEditorElement.tsx
+++ src/editors/cke/CKEditorElement.tsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+
+const config = {
+  plugins: [Paragraph, Bold, Italic, Essentials],
+  toolbar: ["bold", "italic"],
+};
 
 export default function CKEditorElement({ content }: { content: string }) {
   return (
     <CKEditor
       editor={ClassicEditor}
       data={content}
+      config={config}
       onReady={(editor) => {
         // You can store the "editor" and use when it is needed.
         console.log("Editor is ready to use!", editor);
